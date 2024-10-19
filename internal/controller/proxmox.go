1d5cbbe760143a0592b88af8c5daad7afc678c5f
apple <apple@apples-MacBook-Air-m1.local>
2024-10-19 03:58:02 +0200

    .

--- internal/controller/proxmox.go
+++ internal/controller/proxmox.go
@@ -1,39 +1,18 @@
-/*
-Copyright 2020 The Crossplane Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// internal/controller/proxmox.go
 
 package controller
 
 import (
 	"github.com/crossplane/crossplane-runtime/pkg/controller"
-	ctrl "sigs.k8s.io/controller-runtime"
-
 	"github.com/joekky/provider-proxmox/internal/controller/config"
-	// "github.com/joekky/provider-proxmox/internal/controller/mytype"
+	ctrl "sigs.k8s.io/controller-runtime"
+	// Import other controllers as needed
 )
 
-// Setup creates all proxmox controllers with the supplied logger and adds them to
-// the supplied manager.
 func Setup(mgr ctrl.Manager, o controller.Options) error {
-	for _, setup := range []func(ctrl.Manager, controller.Options) error{
-		config.Setup,
-		// mytype.Setup,
-	} {
-		if err := setup(mgr, o); err != nil {
-			return err
-		}
+	if err := config.Setup(mgr, o); err != nil {
+		return err
 	}
+	// Set up other controllers
 	return nil
 }
